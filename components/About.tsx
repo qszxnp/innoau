3abf36fe907b1a44d3b96d82d62987a982424e6f
聂鹏 <niepeng@fengbangleasing.com>
2025-03-10 10:19:22 +0800

    1

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-24 sm:py-32" id="about">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-blue-600">About InnoAU</h2>
